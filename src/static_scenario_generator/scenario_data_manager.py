07b4a6e327fd4cf56e0413313bb3494f11ce8fbe
Colm <colm.p.duffy@gmail.com>
2023-06-15 10:41:30 +0100

    initial package

---
+++ src/static_scenario_generator/scenario_data_manager.py
@@ -0,0 +1,25 @@
+
+
+class DataManager:
+    def __init__(self):
+        self.scenario_columns = ["Scenarios", 
+                                        "Cattle systems", 
+                                        "Manure management", 
+                                        "Dairy pop", 
+                                        "Beef pop", 
+                                        "Dairy prod", 
+                                        "Beef prod", 
+                                        "Forest area", 
+                                        "Conifer proportion",
+                                        "Broadleaf proportion", 
+                                        "Conifer harvest", 
+                                        "Conifer thinned", 
+                                        "Broadleaf harvest", 
+                                        "Crop area",
+                                        "Wetland area", 
+                                        "Grass management", 
+                                        "Upland sheep pop", 
+                                        "Lowland sheep pop", 
+                                        "Dairy Pasture fertilisation", 
+                                        "Beef Pasture fertilisation", 
+                                        "Afforest year"]
---
+++ src/static_scenario_generator/scenarios.py
@@ -0,0 +1,21 @@
+from static_scenario_generator.scenario_data_manager import DataManager
+import pandas as pd
+import json
+
+
+class ScenarioGeneration:
+    def __init__(self):
+        self.data_manager_class = DataManager()
+
+
+    def generate_scenario_dataframe(self, path):
+
+        with open(path) as config_file:
+            config = json.load(config_file)
+
+        columns = self.data_manager_class.scenario_columns
+        #columns = list(config[0].keys())
+
+        scenario_df = pd.DataFrame(config, columns=columns)
+
+        return scenario_df
